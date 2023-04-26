cb7221af986bce5e4966538cc80d9d2db9cf4a4a
Yacqub Mohamed <yam22@cornell.edu>
2023-04-26 14:33:06 -0400

    Perlin New Features
    - Added User Input to Change Seed
    - Added Slider to Change Resolution
    - Refactored User Interface for Clarity
    - Added Documentation for New Functionality

--- bin/perlin.ml
+++ bin/perlin.ml
@@ -156,7 +156,8 @@ let rec grid_fbm x y size n_octaves colorize rand_vals =
   let dmat = distance_matrix (basic_matrix size) size x y in
   pixel_mat_fbm (gray_matrix size) dmat 0 0 size n_octaves colorize rand_vals
 
-let ray_setup () =
+(** [cam_setup ()] is the starting static camera position/angle *)
+let cam_setup () =
   let open Raylib in
   init_window 1000 600 "3D Perlin Noise";
   let camera =
@@ -169,53 +170,124 @@ let ray_setup () =
   set_target_fps 60;
   camera
 
-let draw_perlin camera mat =
+(** [draw_perlin camera mat res] draws the perlin noise on a three-dimensional
+    axis based on the colors specified by [mat] and the starting [camera]
+    position with the specified resolution [res] *)
+let draw_perlin camera mat res =
   begin_drawing ();
-  clear_background Color.blue;
+  clear_background Color.skyblue;
   begin_mode_3d camera;
   let rec draw_seq x y =
     if y >= 600.0 then ()
-    else if x >= 600.0 then draw_seq 0. (y +. 10.0)
+    else if x >= 600.0 then draw_seq 0. (y +. res)
     else
       let mat_entry = Matrix.get_entry (int_of_float x) (int_of_float y) mat in
-      draw_cube (Vector3.create x y 0.0) 10.0 10.0
+      draw_cube (Vector3.create x y 0.0) res res
         (Color.g mat_entry |> float_of_int)
         mat_entry;
-      draw_seq (x +. 10.0) y
+      draw_seq (x +. res) y
   in
   draw_seq 0.0 0.0;
   end_mode_3d ()
 
-let curr_seed = ref 5
+(** [input_seed] is the string representation of the user inputted seed *)
+let input_seed = ref ""
 
-let draw_ui () =
+(** [update_seed_input ()] mutably updates the string holding the user inputted
+    seed *)
+let update_seed_input () =
+  if String.length !input_seed < 7 then
+    match get_key_pressed () with
+    | Key.Zero -> input_seed := !input_seed ^ "0"
+    | Key.One -> input_seed := !input_seed ^ "1"
+    | Key.Two -> input_seed := !input_seed ^ "2"
+    | Key.Three -> input_seed := !input_seed ^ "3"
+    | Key.Four -> input_seed := !input_seed ^ "4"
+    | Key.Five -> input_seed := !input_seed ^ "5"
+    | Key.Six -> input_seed := !input_seed ^ "6"
+    | Key.Seven -> input_seed := !input_seed ^ "7"
+    | Key.Eight -> input_seed := !input_seed ^ "8"
+    | Key.Nine -> input_seed := !input_seed ^ "9"
+    | _ -> ()
+
+(** [input_res] is the starting resolution, or size of the blocks *)
+let input_res = ref 5.
+
+(** [draw_slider curr_res] draws the slider ui with the width specified by
+    [curr_res] *)
+let draw_slider curr_res =
+  draw_text "RESOLUTION" 35 170 15 Color.blue;
+  draw_rectangle 30 200 curr_res 20 Color.blue;
+  draw_rectangle 25 195 5 30 Color.black;
+  draw_rectangle 25 195 115 5 Color.black;
+  draw_rectangle 135 195 5 30 Color.black;
+  draw_rectangle 25 220 115 5 Color.black
+
+let in_slider_range x_pos y_pos =
+  x_pos >= 30 && x_pos <= 130 && y_pos >= 200 && y_pos <= 220
+
+(** [draw_ui seed] creates the user interface to display basic functions and the
+    current [seed] the perlin noise is based on *)
+let draw_ui seed res =
   draw_rectangle 10 10 150 250 Color.white;
-  draw_text ("Current Seed: " ^ string_of_int !curr_seed) 45 50 10 Color.black;
+  draw_text ("Current Seed: " ^ string_of_int seed) 45 50 10 Color.black;
+  draw_text ("Input Seed: " ^ !input_seed) 45 60 10 Color.black;
   draw_text "Press R To" 57 80 10 Color.black;
   draw_text "Change Current Seed" 35 90 10 Color.black;
   draw_text "Drag Mouse To" 46 120 10 Color.black;
   draw_text "Move Camera" 50 130 10 Color.black;
+  draw_slider (int_of_float res);
   end_drawing ()
 
-let new_mat () =
-  grid_fbm 0 0 600 6 convert_grayscale (RTG.gen_random_table !curr_seed)
+(** [new_mat seed] creates a new perlin noise matrix utilizing a random table
+    created from the inputted [seed] *)
+let new_mat seed =
+  grid_fbm 0 0 600 6 convert_grayscale (RTG.gen_random_table seed)
 
-let rec loop mat camera =
+(** [loop mat seed camera] is the main game loop which repeatedly draws the
+    [mat] created by the specified [seed] projected onto 3D space by the
+    [camera] *)
+let rec loop mat seed camera =
   let open Raylib in
   if window_should_close () then close_window ()
+  else if
+    is_mouse_button_down MouseButton.Left
+    && in_slider_range (get_mouse_x ()) (get_mouse_y ())
+  then (
+    (* Slider Logic *)
+    input_res := float_of_int (get_mouse_x () - 25);
+    draw_perlin camera mat !input_res;
+    draw_ui seed !input_res;
+    loop mat seed camera)
   else if is_mouse_button_down MouseButton.Left then (
+    (* Camera Movement Logic *)
     update_camera (addr camera) CameraMode.Third_person;
-    draw_perlin camera mat;
-    draw_ui ();
-    loop mat camera)
+    draw_perlin camera mat !input_res;
+    draw_ui seed !input_res;
+    loop mat seed camera)
   else if is_key_pressed Key.R then (
-    curr_seed := Raylib.get_random_value 0 1000;
-    draw_perlin camera mat;
-    draw_ui ();
-    loop (new_mat ()) camera)
+    let new_seed = Raylib.get_random_value 0 1000 in
+    draw_perlin camera mat !input_res;
+    draw_ui new_seed !input_res;
+    loop (new_mat new_seed) new_seed camera)
+  else if is_key_pressed Key.Enter then (
+    (* Makes the inputted seed display on screen if valid integer *)
+    match int_of_string !input_seed with
+    | s ->
+        input_seed := "";
+        draw_perlin camera mat !input_res;
+        draw_ui seed !input_res;
+        loop (new_mat s) s camera
+    | exception _ ->
+        input_seed := "";
+        draw_perlin camera mat !input_res;
+        draw_ui seed !input_res;
+        loop mat seed camera)
   else (
-    draw_perlin camera mat;
-    draw_ui ();
-    loop mat camera)
+    (* Allows for the user to input a seed, just keeps checking iteratively *)
+    update_seed_input ();
+    draw_perlin camera mat !input_res;
+    draw_ui seed !input_res;
+    loop mat seed camera)
 
-let () = ray_setup () |> loop (new_mat ())
+let () = cam_setup () |> loop (new_mat 5) 5
